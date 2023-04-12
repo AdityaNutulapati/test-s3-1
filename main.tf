cc2b63c2f55899cac2b2302b5f663ec8010c56c4
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-12 13:47:26 +0530

    tt

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
-hello
+variable "VMCount" {
+    description = "How many VMs do you want to start with (number)? default=1 max=5"
+    type = number
+}
+
+output "VMCount" {
+  value = "${var.VMCount > 2 && var.VMCount < 6 ? var.VMCount : 2}"
+}
