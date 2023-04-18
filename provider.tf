e7a3d343eefd19e37ff6ba4e07b1153405760482
AdityaNutulapati <57672073+AdityaNutulapati@users.noreply.github.com>
2023-04-18 11:07:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    iam                       = "http://aws:4566"
+  }
 }
