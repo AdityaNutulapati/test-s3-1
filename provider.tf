b647a97eddc524b1c80c7ed622ff8294c200d45c
AdityaNutulapati <57672073+AdityaNutulapati@users.noreply.github.com>
2023-04-18 14:39:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,7 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.15.0"
-#     }
-#   }
-# }
+provider "aws" {
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
+}
 
 
