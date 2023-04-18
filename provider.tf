3eb6b8c81451fbe3461eba84a8f4f4455fc21f76
AdityaNutulapati <57672073+AdityaNutulapati@users.noreply.github.com>
2023-04-18 11:22:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,13 +7,13 @@ terraform {
   }
 }
 
-provider "aws" {
-  region                      = "us-east-1"
-  s3_use_path_style = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
+# provider "aws" {
+#   region                      = "us-east-1"
+#   s3_use_path_style = true
+#   skip_credentials_validation = true
+#   skip_requesting_account_id  = true
 
-  endpoints {
-    s3                      = "http://aws:4566"
-  }
-}
+#   endpoints {
+#     s3                      = "http://aws:4566"
+#   }
+# }
