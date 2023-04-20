d5df6d2f25737740d7852d35f6a8fe4463c2674f
AdityaNutulapati <57672073+AdityaNutulapati@users.noreply.github.com>
2023-04-20 09:52:12 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -3,9 +3,4 @@ variable "bucketname" {
   type =string
   default = "aditya's bucket"
 }
-# variable "aws_access_key_id" {
-#   type=string
-# }
-# variable "aws_secret_access_key"{
-#   type=string
-# }
+
